2c378a749fd8e3ad80ab0db575aaaed493c84b2c
Segun <folowosele.segun@gmail.com>
2024-08-31 10:55:14 +0400

    multiple pings

--- app/server.go
+++ app/server.go
@@ -22,11 +22,21 @@ func main() {
 		os.Exit(1)
 	}
 	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
 
+	for {
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
 	conn.Write([]byte("+PONG\r\n"))
 }
