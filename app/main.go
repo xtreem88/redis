8e8ce8da28c7f51b3ec66828b56d007e5685ca30
Segun <folowosele.segun@gmail.com>
2024-09-01 13:26:38 +0400

    custom port

--- app/main.go
+++ app/main.go
@@ -11,8 +11,9 @@ import (
 func main() {
 	dir := flag.String("dir", "", "The directory where RDB files are stored")
 	dbfilename := flag.String("dbfilename", "", "The name of the RDB file")
+	port := flag.Int("port", 6379, "The port to run redis on")
 	flag.Parse()
-	s, err := server.New("0.0.0.0", 6379, *dir, *dbfilename)
+	s, err := server.New("0.0.0.0", *port, *dir, *dbfilename)
 	if err != nil {
 		fmt.Printf("Failed to create server: %v\n", err)
 		os.Exit(1)
